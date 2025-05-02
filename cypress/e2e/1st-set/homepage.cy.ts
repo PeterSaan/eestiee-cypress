5d0357ea5a4495473f1b2c00b421f218d7ebd12d
PeterSaan <saanpeter@gmail.com>
2025-05-02 23:47:30 +0300

    Finish 1st set

--- cypress/e2e/1st-set/homepage.cy.ts
+++ cypress/e2e/1st-set/homepage.cy.ts
@@ -20,8 +20,8 @@ describe("Load homepage and it's elements", () => {
 	it('Menupoints are present and functional', () => {
 		cy.visit('https://www.eesti.ee');
 
-		cy.get('div.sidenav-menu-container').contains('Üldinfo').get('div>ul').each((e) => {
-			
-		})
+		cy.get('div.sidenav-menu-container > div:nth-of-type(3) > div > div:nth-of-type(2) > ul').should('have.length', 7);
+		cy.get('div.sidenav-menu-container > div:nth-of-type(4) > div > div:nth-of-type(2) > ul').should('have.length', 12)
+		cy.get('div.sidenav-menu-container > div:nth-of-type(5) > div > div:nth-of-type(2) > ul').should('have.length', 9)
 	})
 });
